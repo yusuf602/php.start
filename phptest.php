b9b1f6e6edabf94aa98c92eae811c4520b15f6bd
yusuf602 <99082369@mydavinci.nl>
2025-06-03 10:31:16 +0200

    php start opdrachten
    dit zijn school opdrachten

---
+++ phptest.php
@@ -0,0 +1,54 @@
+<?php
+date_default_timezone_set('Europe/Amsterdam');
+
+$uur = date("H");
+$tijd = date("H:i");
+
+$achtergrond = "img/morning.png";
+$groet = "Goede morgen";
+
+if ($uur >= 6 && $uur < 12) {
+    $achtergrond = "img/morning.png";
+    $groet = "Goede morgen";
+} elseif ($uur >= 12 && $uur < 18) {
+    $achtergrond = "img/afternoon.png";
+    $groet = "Goede middag";
+} elseif ($uur >= 18 && $uur < 24) {
+    $achtergrond = "img/evening.png";
+    $groet = "Goede avond";
+} else {
+    $achtergrond = "img/night.png";
+    $groet = "Goede nacht";
+}
+?>
+
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <title>Dynamische Welkomstpagina</title>
+    <style>
+        body {
+            background-image: url('<?php echo $achtergrond; ?>');
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-position: center;
+            color: white;
+            font-family: Arial, sans-serif;
+            text-align: center;
+            padding-top: 20%;
+        }
+
+        h1 {
+            font-size: 3em;
+            background-color: rgba(0,0,0,0.5);
+            display: inline-block;
+            padding: 20px;
+            border-radius: 10px;
+        }
+    </style>
+</head>
+<body>
+    <h1><?php echo $groet; ?>!<br>Het is nu <?php echo $tijd; ?></h1>
+</body>
+</html>
